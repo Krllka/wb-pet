a3a17ac31808a88e9a34b6a11a58cdfb593ca1a9
Krll <k.r.llaleks25@gmail.com>
2023-08-01 22:27:24 +0400

    Init project

---
+++ src/main/java/com/wbpet/WbPetApplication.java
@@ -0,0 +1,13 @@
+package com.wbpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbpet/WbPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
